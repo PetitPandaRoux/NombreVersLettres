385538a595006b2b2860764fda2379101666bd87
Sakada Ly <lysakada@gmail.com>
2019-08-27 01:28:40 +0200

    exercices with dd done

--- createDOM.js
+++ createDOM.js
@@ -44,7 +44,26 @@ function createTextNode(texte, idToAppend){
   monTag.appendChild(monTexte); 
 }
 
-function createUlWithLiWithTexte( cible, array){
+
+
+function createTableOfDefinition(dict, cible){
+  let nouveauTag = document.createElement("dl");
+  for (let cle in dict) {
+    if (dict.hasOwnProperty(cle)) {
+      let nouveauDt = document.createElement("dt");
+      let nouveauTitre = document.createTextNode(cle);
+      nouveauDt.appendChild(nouveauTitre);  
+      nouveauTag.appendChild(nouveauDt);
+      let nouveauDd = document.createElement("dd");
+      let nouvelleDefinition= document.createTextNode(dict[cle]);
+      nouveauDd.appendChild(nouvelleDefinition); 
+      nouveauTag.appendChild(nouveauDd);
+    }
+  document.getElementById(cible).appendChild(nouveauTag);
+  } 
+}
+
+function createUlWithLiWithTexte(array, cible){
   let nouveauTag = document.createElement("ul"); 
   for (var element in array){
     let liChild = document.createElement('li');
@@ -52,7 +71,6 @@ function createUlWithLiWithTexte( cible, array){
     liChild.appendChild(liElement);
     nouveauTag.appendChild(liChild);
   }
-
   document.getElementById(cible).appendChild(nouveauTag);
 
 }
@@ -70,5 +88,18 @@ function creerDOM(){
 
   createDivAvecId("", 'exo2', 'divTP2');
   createNodeWithTexte('p','langages basés sur ECMAScript','divTP2');
-  createUlWithLiWithTexte('divTP2', ['Javascript', 'JScript', 'ActionScript', 'EX4']);
+  createUlWithLiWithTexte(['Javascript', 'JScript', 'ActionScript', 'EX4'], 'divTP2' );
+
+
+  var definitionLangage = {
+    'javascript': "Javascript est un langage de programmation de scripts principalement utilisé dans les pages web interactives mais aussi côté serveur",
+    'JScript': "JScript est le nm générique de plusieurs implémentatoins d'ECMAscript 3 créees par Microsoft",
+    'ActionScript': "ActionScript est le langage de programmation au sein d'applications clients (Adobe Flash, Adoble Flex) et serveur (Flash media server, JRun, Macromédia Générator).",
+    'EX4': "ECMAScript for XML(E4X) est une extension XML au langage ECMAScript."
+  };
+
+  createDivAvecId("", 'exo3', 'divTP3');
+  createNodeWithTexte('p', 'Langages basés sur ECMAScript :', 'divTP3');
+  createTableOfDefinition(definitionLangage, 'divTP3');
+
 }
